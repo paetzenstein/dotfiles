5c6c1d90f3d624a22e74f8c3b894cf7bef570c35
paetzeinstein <paetzenstein@mailbox.org>
2022-11-18 00:29:35 +0100

    added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,39 @@
+set -U fish_greeting "🐟"
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+# NVM
+function __check_rvm --on-variable PWD --description 'Do nvm stuff'
+  status --is-command-substitution; and return
+
+  if test -f .nvmrc; and test -r .nvmrc;
+    nvm use
+  else
+  end
+end
+
+# Exa
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+end
+starship init fish | source
